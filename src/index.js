22fed9fb25f8a60664f26b00407f277a92e6d925
nkcube <nithin@cubereum.com>
2018-08-01 14:51:44 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+var bodyParser = require('body-parser')
+
+//Attach the middleware
+app.use( bodyParser.json() );
+
+const ExampleNetwork = require('./ExampleNetwork');
+app.post('/api/sell', function(req, res) {
+        var data = req.body.data;
+        var exampleNetwork = new ExampleNetwork('admin');
+
+exampleNetwork.init().then(function(data) {
+          return exampleNetwork.sell(data)
+        }).then(function (data) {
+          res.status(200).json(data)
+        }).catch(function(err) {
+          res.status(500).json({error: err.toString()})
+        })
+})
